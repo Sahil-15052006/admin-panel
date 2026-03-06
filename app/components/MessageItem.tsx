cad4780eb8c1c16be33ca851871a936741174625
sahil <sahilsawant15052006@gmail.com>
2026-03-06 18:17:47 +0530

    fix resume url null src issue

--- app/components/MessageItem.tsx
+++ app/components/MessageItem.tsx
@@ -1,14 +1,32 @@
 import { supabase } from '@/lib/supabase'
 import { Reply, Trash2 } from 'lucide-react'
-import React from 'react'
+import { useRouter } from 'next/navigation'
 
 interface messageType{
+  id:number,
   name:string,
   email:string,
   content:string
 }
 
 export default function MessageItem({message}:{message:messageType}) {
+
+  const router = useRouter()
+
+  const handleDelete=async()=>{
+    try{
+      const {error} = await supabase
+      .from("messages")
+      .delete()
+      .eq("id",message.id)
+      if (error) throw error;
+            console.log('Message Deleted');
+            router.refresh()
+      } catch (err) {
+            console.error(err)
+            alert("Failed delete the message")
+    }  
+  }
 
   return (
     <div className='slideright flex items-start p-1 sm:p-2 border border-(--secondary)/40 hover:border-(--primary) w-full hover:h-fit space-x-2 rounded-2xl group transition duration-300'>
@@ -22,11 +40,9 @@ export default function MessageItem({message}:{message:messageType}) {
               <div>{message.email}</div>
               <div className='flex flex-wrap'>{message.content}</div>
               <div className='flex flex-row space-x-2 p-2 transition duration-300 w-full '>
-                <button className='flex hover:bg-(--primary)/20 rounded-xl border border-(--primary)/10 hover:scale-105 p-2 space-x-2 justify-center items-start transition duration-300'>
-                  <span>Reply</span>
-                  <Reply/>
-                </button>
-                <Trash2 className='flex hover:bg-(--primary)/20 rounded-xl border border-(--primary)/10 hover:scale-105 p-2 h-10 w-10 transition duration-300 '/>
+                <Trash2 
+                onClick={handleDelete}
+                className='flex hover:bg-(--primary)/20 rounded-xl border border-(--primary)/10 hover:scale-105 p-2 h-10 w-10 transition duration-300 '/>
               </div>
             </div>
     </div>
